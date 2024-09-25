1829f455a4de4722098f44eed64660cf6170b2f4
ErleyMDR <erley.monteiro@aluno.uece.br>
2024-09-25 20:53:03 -0300

    Update Main.py
    Adicionei um trecho para pegar o absolute path do banco de dados

--- Gerenciamento_Funcionario/Main.py
+++ Gerenciamento_Funcionario/Main.py
@@ -1,4 +1,8 @@
+import os.path
 from FrontEnd import Frontend
+
+Base_Dir = os.path.dirname(__file__)
+db_path = os.path.join(Base_Dir, "gerenciamento_funcionarios.db")
 
 if __name__ == "__main__":
     app = Frontend()
